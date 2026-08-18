e693b8528b5c21b9267583374c406ad5634bcb44
Allan O <allanornel@gmail.com>
2026-08-18 19:20:03 -0300

    init back repository

---
+++ back/avaliacao-mnr/src/test/java/ifrs/edu/avaliacao_mnr/AvaliacaoMnrApplicationTests.java
@@ -0,0 +1,13 @@
+package ifrs.edu.avaliacao_mnr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoMnrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
