5bdd0b70395f856864d7db47060755312cc0a6f8
Taylan <taylan.hahn@gmail.com>
2026-08-21 14:18:18 -0300

    update classes

--- back/avaliacao-mnr/src/main/java/ifrs/edu/avaliacao_mnr/project/dto/ProjectImportDTO.java
+++ back/avaliacao-mnr/src/main/java/ifrs/edu/avaliacao_mnr/project/dto/ProjectImportDTO.java
@@ -3,7 +3,8 @@ package ifrs.edu.avaliacao_mnr.project.dto;
 /* T: Optei por RECORD, porque estava relendo algumas anotações
 e vi que é um tipo especial de classe, usada para representar dados imutáveis.
 Fica mais enxuto e abstraído, ideal para classes que só carregam dados, mas
-não tem lógica complexa. O construtor, getters e setters, equals, hashcode e toString são gerados automaticamente.
+não tem lógica complexa. O construtor, getters e setters, equals, hashcode e
+toString são gerados automaticamente.
 */
 public record ProjectImportDTO(
         String registrationNumber,
