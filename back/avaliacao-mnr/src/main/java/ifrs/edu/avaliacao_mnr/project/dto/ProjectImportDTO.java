9cbf745543a084897843f4fc6c5c3b1c05e86253
Taylan <taylan.hahn@gmail.com>
2026-08-21 01:50:48 -0300

    feat: update DTO to record, add CSV dependency and initial docs to service

--- back/avaliacao-mnr/src/main/java/ifrs/edu/avaliacao_mnr/project/dto/ProjectImportDTO.java
+++ back/avaliacao-mnr/src/main/java/ifrs/edu/avaliacao_mnr/project/dto/ProjectImportDTO.java
@@ -1,5 +1,16 @@
 package ifrs.edu.avaliacao_mnr.project.dto;
 
-public class ProjectImportDTO {
-    // código a ser desenvolvido
+/* T: Optei por RECORD, porque estava relendo algumas anotações
+e vi que é um tipo especial de classe, usada para representar dados imutáveis.
+Fica mais enxuto e abstraído, ideal para classes que só carregam dados, mas
+não tem lógica complexa. O construtor, getters e setters, equals, hashcode e toString são gerados automaticamente.
+*/
+public record ProjectImportDTO(
+        String registrationNumber,
+        String name,
+        String team,
+        String level,
+        String videoUrl,
+        String institution
+) {
 }
